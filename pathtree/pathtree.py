1963cfc21141277ddf4bad37068c7cf7551e336f
aloyszheng <aloyszheng@tencent.com>
2021-09-09 15:12:33 +0800

    support other split char for path， first new a Tree(split_charr='#'), and then use makepath() to build the path.

--- pathtree/pathtree.py
+++ pathtree/pathtree.py
@@ -12,26 +12,29 @@ class Leaf(object):
         self.order = order
 
 
-def Tree(logger=logging):
-    return Node({}, [], {}, 0, logger)
+def Tree(logger=logging, split_char="/"):
+    return Node({}, [], {}, 0, logger, split_char)
 
 
 class Node(object):
-    def __init__(self, edges, leaf, extra_data, leaf_cnt=0, logger=logging):
+    def __init__(self, edges, leaf, extra_data, leaf_cnt=0,
+                 logger=logging, split_char="/"):
         self.extra_data = extra_data
         self.edges = edges
         self.leaf = leaf
         self.leaf_cnt = leaf_cnt
         self.logger = logger
+        self.split_char = split_char
 
     def Add(self, path, val, extra_data=None):
         if extra_data is None:
             extra_data = {}
-        if not path or not path.startswith('/'):
-            self.logger.error("Path must begin with /")
-            return None, "Path must begin with /"
+        if not path or not path.startswith(self.split_char):
+            self.logger.error("Path must begin with %s" % self.split_char)
+            return None, "Path must begin with %s" % self.split_char
         self.leaf_cnt += 1
-        return self.add(self.leaf_cnt, self.splitPath(path), val, extra_data)
+        return self.add(self.leaf_cnt, self.splitPath(path, self.split_char),
+                        val, extra_data)
 
     def addLeaf(self, leaf):
         if not self.leaf:
@@ -61,14 +64,14 @@ class Node(object):
         return e.add(order, elements, val, extra_data)
 
     def Find(self, path):
-        if len(path) == 0 or path[0] != "/":
+        if len(path) == 0 or path[0] != self.split_char:
             return None, None
-        return self.find(self.splitPath(path))
+        return self.find(self.splitPath(path, self.split_char))
 
     def FindLeaf(self, path, value):
-        if len(path) == 0 or path[0] != "/":
+        if len(path) == 0 or path[0] != self.split_char:
             return None, None
-        leafs = self.find(self.splitPath(path))
+        leafs = self.find(self.splitPath(path, self.split_char))
         for leaf in leafs:
             if leaf.value == value:
                 return leaf
@@ -86,9 +89,9 @@ class Node(object):
         return leaf
 
     def FindPath(self, path):
-        if len(path) == 0 or path[0] != "/":
+        if len(path) == 0 or path[0] != self.split_char:
             return None
-        return self.find_path(self.splitPath(path))
+        return self.find_path(self.splitPath(path, self.split_char))
 
     def find_path(self, elements):
         target_node = None
@@ -108,9 +111,10 @@ class Node(object):
         return target_node
 
     def DeleteLeaf(self, path, value):
-        if len(path) == 0 or path[0] != "/":
+        if len(path) == 0 or path[0] != self.split_char:
             return None, None
-        leaf, target_node = self.delete_leaf(self.splitPath(path), value)
+        leaf, target_node = self.delete_leaf(
+            self.splitPath(path, self.split_char), value)
         return leaf, target_node
 
     @staticmethod
@@ -147,10 +151,11 @@ class Node(object):
         return leaf, target_node
 
     def DeletePath(self, path):
-        if not path or path == "/" or path[0] != "/":
+        if not path or path == self.split_char or path[0] != self.split_char:
             self.logger.error("path %s is invalid")
             return None, None
-        target_node, father_node = self.delete_path(self.splitPath(path))
+        target_node, father_node = self.delete_path(
+            self.splitPath(path, self.split_char))
         return target_node, father_node
 
     def delete_path(self, elements):
@@ -201,8 +206,8 @@ class Node(object):
             return "", ""
 
     @staticmethod
-    def splitPath(path):
-        elements = path.split("/")
+    def splitPath(path, split_char="/"):
+        elements = path.split(split_char)
         if elements[0] == "":
             elements = elements[1:]
         if elements[len(elements) - 1] == "":
@@ -236,6 +241,9 @@ class Node(object):
         """return if list a contain list b """
         ret = [False for i in list_b if i not in list_a]
         return False if ret else True
+
+    def makepath(self, *args):
+        return self.split_char.join(args)
 
 
 def found(n, p, val):
@@ -280,7 +288,6 @@ def notfound_path(n, path):
 def Testing():
     print("\n============ TestColon ============")
     n = Tree()
-
     n.Add("/", "root", extra_data={"test": "just a test"})
     n.Add("/", "root1")
 
@@ -370,5 +377,98 @@ def Testing():
     notfound_path(n, "/f/g/h")
 
 
+def Testing_other_split_char():
+    print("\n============ TestColon With Other Split Char  ============")
+    n = Tree(split_char="|")
+    n.Add("|", "root", extra_data={"test": "just a test"})
+    n.Add("|", "root1")
+
+    n.Add("|a", None)
+    n.Add("|a", "a")
+    n.Add("|a", "aa")
+    n.Add("|a", "aaa")
+
+    n.Add("|a|b", "b")
+    n.Add("|a|b", "bb")
+    n.Add("|a|b", "bbb")
+
+    n.Add("|a|b|c", "c")
+    n.Add("|a|b|c", "cc")
+    n.Add("|a|b|c", "ccc")
+
+    n.Add("|a|d", "d")
+    n.Add("|a|d", "dd")
+    n.Add("|a|d", "ddd")
+
+    n.Add("|a|d|e", "e")
+    n.Add("|a|d|e", "ee")
+    n.Add("|a|d|e", "eee")
+
+    n.Add("|f|g|h", "h")
+    n.Add("|f|g|h", "hh")
+    n.Add("|f|g|h", "hhh")
+
+    n.SetPathExtraData("|a", {"test": "hi"})
+
+    found(n, "|", "root")
+    found(n, "|", "root1")
+    found(n, "|a", None)
+    found(n, "|a", "a")
+    found(n, "|a", "aa")
+    found(n, "|a", "aaa")
+    found(n, "|a|b", "b")
+    found(n, "|a|b", "bb")
+    found(n, "|a|b", "bbb")
+    found(n, "|a|b|c", "c")
+    found(n, "|a|b|c", "cc")
+    found(n, "|a|b|c", "ccc")
+    found(n, "|a|d", "d")
+    found(n, "|a|d", "dd")
+    found(n, "|a|d", "ddd")
+    found(n, "|a|d|e", "e")
+    found(n, "|a|d|e", "ee")
+    found(n, "|a|d|e", "eee")
+    found(n, "|f|g|h", "h")
+    found(n, "|f|g|h", "hh")
+    found(n, "|f|g|h", "hhh")
+
+    notfound(n, "|a", "!a")
+    notfound(n, "|a|e", "!a")
+
+    n.DeleteLeaf("|", "root")
+    notfound(n, "|", "root")
+
+    n.DeleteLeaf("|a", "a")
+    notfound(n, "|a", "a")
+    found(n, "|a", "aa")
+    found(n, "|a", "aaa")
+
+    n.DeleteLeaf("|a|b", "b")
+    notfound(n, "|a|b", "b")
+    found(n, "|a|b", "bb")
+    found(n, "|a|b", "bbb")
+
+    n.DeleteLeaf("|a|b", "b")
+    notfound(n, "|a|g|h", "h")
+
+    n.DeletePath("|a")
+    notfound_path(n, "|a")
+    notfound_path(n, "|a|b")
+    notfound_path(n, "|a|b|c")
+    notfound_path(n, "|a|d")
+    notfound_path(n, "|a|d|e")
+
+    n.DeletePath("|f|g|h")
+    notfound_path(n, "|f|g|h")
+    found_path(n, "|f")
+    found_path(n, "|f|g")
+
+    n.DeletePath("|f")
+    notfound_path(n, "|f")
+    notfound_path(n, "|f|g")
+    notfound_path(n, "|f|g|h")
+
+
 if __name__ == "__main__":
     Testing()
+    Testing_other_split_char()
